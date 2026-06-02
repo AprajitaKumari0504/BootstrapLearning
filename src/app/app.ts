d893becdd78083b48f4457e8111687433f16bc3a
Aprajita Kumari <aprajitakumari0504@gmail.com>
2026-06-03 00:52:37 +0530

    login component created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { Login } from './login/login';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink,CommonModule,Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
